69f0729576fe83905de52e7d3b85dc02069b37be
Sam <sammm.sh@gmail.com>
2021-02-05 15:29:25 +0100

    skeleton javafx+jfoenix using maven

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module software.design.vu {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires com.jfoenix;
+
+    opens softwaredesign;
+    opens softwaredesign.controller;
+}
---
+++ src/main/java/softwaredesign/controller/LoginContoller.java
@@ -0,0 +1,11 @@
+package softwaredesign.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
+public class LoginContoller {
+
+    @FXML
+    private JFXButton fxbtn;
+
+}
