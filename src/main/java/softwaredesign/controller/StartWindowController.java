735f8dca45e24b29612bb102ff9ca232f1b617bf
Sam <sammm.sh@gmail.com>
2021-02-05 20:11:33 +0100

    Add basic start page

--- src/main/java/softwaredesign/controller/StartWindowController.java
+++ src/main/java/softwaredesign/controller/StartWindowController.java
@@ -3,9 +3,6 @@ package softwaredesign.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 
-public class LoginContoller {
-
-    @FXML
-    private JFXButton fxbtn;
+public class StartWindowController {
 
 }
