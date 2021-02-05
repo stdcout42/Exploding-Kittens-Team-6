d4db230dcc12d4029739eeba5bf5410c725159e2
Sam <sammm.sh@gmail.com>
2021-02-05 15:29:25 +0100

    skeleton javafx+jfoenix using maven

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,7 +1,19 @@
 package softwaredesign;
 
-public class Main {
-    public static void main (String[] args){
-        System.out.println("Welcome to Software Design");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("/LoginWindow.fxml"));
+        Scene scene = new Scene(parent);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
