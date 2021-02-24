650f883e6f3a770c06be1a7ef0415ad715a7f9d6
Sam <sammm.sh@gmail.com>
2021-02-24 21:02:52 +0100

    Add BotAvatar functionality & ability to select bot avatars (for steal card feature)

--- src/main/java/softwaredesign/controller/GameWindowController.java
+++ src/main/java/softwaredesign/controller/GameWindowController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import softwaredesign.Game;
+import softwaredesign.model.Card;
+import softwaredesign.view.BotAvatar;
 import softwaredesign.view.ViewFactory;
 
 import java.net.URL;
@@ -31,11 +33,26 @@ public class GameWindowController extends WindowControler{
     @FXML
     private Label cardSelectLabel;
 
+    @FXML
+    private Label playerSelectLabel;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         game.setupGame(playerCardHBox.getChildren());
+        for (int i  = 2; i <= game.getNumberOfPlayers(); i++)
+            botHBox.getChildren().add(new BotAvatar(i));
+
+        updateBotAvatarListClickListeners();
         updateHumanCardListClickListeners();
         initLogTextArea();
+    }
+
+    private void updateBotAvatarListClickListeners() {
+        for(Node botAvatar : botHBox.getChildren()) {
+            botAvatar.setOnMouseClicked(mouseEvent -> {
+                playerSelectLabel.setText(botAvatar.toString() + " selected.");
+            });
+        }
     }
 
     private void initLogTextArea() {
@@ -47,16 +64,22 @@ public class GameWindowController extends WindowControler{
     private void updateHumanCardListClickListeners() {
         for (Node card : playerCardHBox.getChildren()) {
             card.setOnMouseClicked(mouseEvent -> {
-                cardSelectLabel.setText(card.toString() + " selected");
+                cardSelectLabel.setText(card.toString() + " card selected");
             });
         }
     }
 
     @FXML
     void drawButtonAction() {
+        Card drawnCard;
         if(game.getTurnNumber() == 1) {
-            appendToLog("You drew a card...");
-            game.playerDraws();
+            drawnCard = game.playerDraws();
+            appendToLog("You drew a " + drawnCard.toString() + " card");
+            if(drawnCard.cardType == Card.CardType.EXPLODING_KITTEN) {
+                //TODO
+            } else {
+                updateHumanCardListClickListeners();
+            }
         }
         else appendToLog("It's not your turn yet...");
     }
